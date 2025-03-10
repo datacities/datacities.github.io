b49df230f8c173da6d06e60606f918eaaae9c943
datacities <datacities@users.noreply.github.com>
2025-03-10 08:17:25 +0000

    deploy: 8262f4652c5cc1d3a1b3142bd8d48243c0c4bda7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,25 +1,25 @@
 var store = [{
         "title": "Basel, Switzerland",
-        "excerpt":"Focus Areas   Data Governance   Data Catalog   Data Science and AI   Data Culture and Competencies   Implementation  For the implementation of the data strategy, Basel has created a Data Competence Center. The implementation can be followed at data.bs.  ","categories": [],
+        "excerpt":"Basel has a new data strategy.   Focus Areas   Data Governance   Data Catalog   Data Science and AI   Data Culture and Competencies   Implementation  For the implementation of the data strategy, Basel has created a Data Competence Center. The implementation can be followed at data.bs.  ","categories": [],
         "tags": [],
-        "url": "/cities/basel.html",
+        "url": "/strategies/basel.html",
         "teaser": null
       },{
         "title": "Helsinki, Finland",
         "excerpt":"Overview  Helsinki’s data strategy aims to create an open and transparent city by making data accessible and encouraging the development of smart city solutions. Open data platforms and public-private partnerships are key components of this strategy.   ","categories": [],
         "tags": [],
-        "url": "/cities/helsinki.html",
+        "url": "/strategies/helsinki.html",
         "teaser": null
       },{
         "title": "New York City, USA",
         "excerpt":"Overview  New York’s data strategy revolves around improving public services through open data initiatives and encouraging civic engagement.   ","categories": [],
         "tags": [],
-        "url": "/cities/new_york.html",
+        "url": "/strategies/new_york.html",
         "teaser": null
       },{
         "title": "Zurich, Switzerland",
         "excerpt":"Overview  Zurich has implemented a comprehensive data strategy aimed at enhancing city services, improving governance, and fostering transparency. The city’s open data platform serves as the foundation for building innovative public services.   ","categories": [],
         "tags": [],
-        "url": "/cities/zurich.html",
+        "url": "/strategies/zurich.html",
         "teaser": null
-      },]
+      }]
