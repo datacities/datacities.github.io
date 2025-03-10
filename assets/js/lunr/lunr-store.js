347818686079d8a0eb8270a23224fa599211f91c
datacities <datacities@users.noreply.github.com>
2025-03-10 09:20:21 +0000

    deploy: 477048788ef37b15f71b12d089d35792630d6575

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/strategies/basel.html",
         "teaser": null
       },{
+        "title": "Bonn, Germany",
+        "excerpt":"Introduction and Context Bonn’s Data Strategy outlines a framework for handling urban data, aiming to establish a high level of data quality, transparency, and availability. The strategy is designed to support the city’s digital transformation and enable evidence-based policymaking. It builds on previous open data initiatives, including the city’s Open...","categories": [],
+        "tags": [],
+        "url": "/strategies/bonn.html",
+        "teaser": null
+      },{
         "title": "Helsinki, Finland",
         "excerpt":"Introduction and Context The Helsinki Data Strategy is part of the city’s broader Digitalisation Programme, approved in March 2019. The goal is to make Helsinki “the most functional city in the world” by leveraging data, artificial intelligence, and robotisation to improve public services and decision-making. Vision and Objectives Vision: Helsinki...","categories": [],
         "tags": [],
@@ -15,6 +21,12 @@ var store = [{
         "excerpt":"Overview  New York’s data strategy revolves around improving public services through open data initiatives and encouraging civic engagement.   ","categories": [],
         "tags": [],
         "url": "/strategies/new_york.html",
+        "teaser": null
+      },{
+        "title": "Vienna, Austria",
+        "excerpt":"Introduction and Context The Data Excellence Strategy (DX) 2.1 is Vienna’s strategic framework for data governance and management, replacing the previous IKT-Teilstrategie Data Excellence from March 1, 2019. The strategy reflects Vienna’s goal to leverage data as a strategic asset for efficient public service delivery, informed decision-making, and fostering innovation....","categories": [],
+        "tags": [],
+        "url": "/strategies/vienna.html",
         "teaser": null
       },{
         "title": "Zurich, Switzerland",
