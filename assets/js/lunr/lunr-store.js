b853e4efd1ba766f341b38b0b32cba82f4bebf9c
datacities <datacities@users.noreply.github.com>
2025-03-10 06:52:41 +0000

    deploy: e3a4fd0fb92efcbd244a8e57897c62518a5581c3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Helsinki, Finland",
+        "excerpt":"Overview  Helsinki’s data strategy aims to create an open and transparent city by making data accessible and encouraging the development of smart city solutions. Open data platforms and public-private partnerships are key components of this strategy.   ","categories": [],
+        "tags": [],
+        "url": "/cities/helsinki.html",
+        "teaser": null
+      },{
+        "title": "New York City, USA",
+        "excerpt":"Overview  New York’s data strategy revolves around improving public services through open data initiatives and encouraging civic engagement.   ","categories": [],
+        "tags": [],
+        "url": "/cities/new_york.html",
+        "teaser": null
+      },{
+        "title": "Zurich, Switzerland",
+        "excerpt":"Overview  Zurich has implemented a comprehensive data strategy aimed at enhancing city services, improving governance, and fostering transparency. The city’s open data platform serves as the foundation for building innovative public services.   ","categories": [],
+        "tags": [],
+        "url": "/cities/zurich.html",
+        "teaser": null
+      },]
